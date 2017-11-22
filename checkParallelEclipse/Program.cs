56df2db2e7b9d2b56868914fdc02941b1c122d74
PetroAus <kumar.sankararaj@petro-aus.com>
2017-11-22 11:40:09 +0800

    Files Added

---
+++ checkParallelEclipse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace checkParallelEclipse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkParallel= false;
+            string filename = "BM_with_Prelude.DATA";
+
+            string[] lines = File.ReadAllLines(filename);
+            foreach (var line in lines)
+            {
+                var newline = line.ToUpper();
+                newline = newline.Trim();
+                if (newline.StartsWith("PARALLEL"))
+                {
+                    checkParallel = true;
+                }
+            }
+            //return checkParallel;
+            if (checkParallel)
+            {
+                Console.WriteLine("Eclipse is parallel");
+            }
+            else
+            {
+                Console.WriteLine("Eclipse is NOT parallel");
+            }
+            Console.ReadKey();
+        }
+    }
+}
